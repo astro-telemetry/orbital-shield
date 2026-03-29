c010e77b2b8526b58f508ee212bc164057b1596b
space-bytes <ianlindell@icloud.com>
2026-03-29 13:21:14 -0400

    feat: implement json marshaling for telemetry feature

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
+	"time"
 )
+
+type InterfaceReport struct {
+	Timestamp     int64    `json:"timestamp"`
+	InterfaceName string   `json:"interface_name"`
+	IsActive      bool     `json:"is_active"`
+	MTU           int      `json:"mtu"`
+	IPs           []string `json:"ip_list"`
+}
 
 func main() {
 	fmt.Println("--- Orbital Shield: Network Status Monitor ---")
@@ -14,13 +24,20 @@ func main() {
 		return
 	}
 
+	interface_reports := make([]InterfaceReport, 5)
+
 	for _, iface := range interfaces {
 		// Bitwise check: Is the 'Up' flag set?
 		isActive := iface.Flags&net.FlagUp != 0
+		report := InterfaceReport{
+			Timestamp:     time.Now().Unix(),
+			InterfaceName: iface.Name,
+			IsActive:      isActive}
 
 		if isActive {
+			report.MTU = iface.MTU
 			fmt.Printf("[ACTIVE]  Interface: %-10s (MTU: %d)\n", iface.Name, iface.MTU)
-
+			ips := []string{}
 			addrs, err := iface.Addrs()
 			if err != nil {
 				fmt.Printf("  ! Error: %v\n", err)
@@ -28,11 +45,21 @@ func main() {
 			}
 			for _, addr := range addrs {
 				fmt.Printf("  -> Addr: %s\n", addr.String())
+				ips = append(ips, addr.String())
 			}
+			report.IPs = ips
 		} else {
 			// Documenting the 'Down' cards
 			fmt.Printf("[OFFLINE] Interface: %-10s (Hardware Check Recommended)\n", iface.Name)
 		}
 		fmt.Println("----------------------------------------------")
+		interface_reports = append(interface_reports, report)
+
+	}
+	formatted_reports, err := json.MarshalIndent(interface_reports, "", "  ")
+	if err != nil {
+		fmt.Println(string(formatted_reports))
+	} else {
+		fmt.Printf("	! Error: %v\n", err)
 	}
 }
