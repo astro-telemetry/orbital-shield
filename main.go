f241fc89f48e064bf91f417c97c0f96f6892b255
space-bytes <ianlindell@icloud.com>
2026-03-31 10:06:37 -0400

    feat: collecting logs in json file

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -18,55 +20,92 @@ type InterfaceReport struct {
 	ErrorLog      string   `json:"error_log,omitempty"`
 }
 
+var sanitizeRegex = regexp.MustCompile("[^a-z0-9]+")
+
+func Sanitize(input string) string {
+	output := strings.ToLower(input)
+	output = sanitizeRegex.ReplaceAllString(output, "-")
+	return strings.Trim(output, "-")
+}
+
 func CollectTelemetry() ([]InterfaceReport, error) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		return nil, err
 	}
 
+	timestamp := time.Now().Unix()
 	interfaces, err := net.Interfaces()
 	if err != nil {
 		return nil, err
 	}
 
 	interface_reports := make([]InterfaceReport, 0)
-	report := InterfaceReport{
-		HostID:    hostname,
-		Timestamp: time.Now().Unix(),
-	}
 
 	for _, iface := range interfaces {
 		// Bitwise check: Is the 'Up' flag set?
 		isActive := iface.Flags&net.FlagUp != 0
-		report.InterfaceName = iface.Name
-		report.IsActive = isActive
+		report := InterfaceReport{
+			HostID:        hostname,
+			Timestamp:     timestamp,
+			InterfaceName: iface.Name,
+			IsActive:      isActive,
+		}
 
 		if isActive {
 			report.MTU = iface.MTU
-			ips := []string{}
 			addrs, err := iface.Addrs()
 			if err != nil {
 				report.ErrorLog = err.Error()
-				interface_reports = append(interface_reports, report)
-				continue
+			} else {
+				ips := []string{}
+				for _, addr := range addrs {
+					ips = append(ips, addr.String())
+				}
+				report.IPs = ips
 			}
-			for _, addr := range addrs {
-				ips = append(ips, addr.String())
-			}
-			report.IPs = ips
 		}
 		interface_reports = append(interface_reports, report)
 	}
-	return interface_reports, err
+	return interface_reports, nil
 }
 
 func main() {
+	// create directory
+	os.MkdirAll("telemetry_logs", 0755)
 
-	formatted_reports, err := json.MarshalIndent(CollectTelemetry, "", "  ")
+	// collect data
+	collect_telemetry, err := CollectTelemetry()
+	if err != nil {
+		fmt.Printf("Collector Error: %v\n", err)
+		os.Exit(1)
+	}
 
+	// ensure list isn't empty
+	if len(collect_telemetry) == 0 {
+		fmt.Println("No telemetry data captured.")
+		return
+	}
+
+	// marshal to json
+	formatted_reports, err := json.MarshalIndent(collect_telemetry, "", "  ")
 	if err != nil {
 		fmt.Printf("	! Error: %v\n", err)
-	} else {
-		fmt.Println(string(formatted_reports))
+		return
 	}
+
+	// create filename
+	host := Sanitize(collect_telemetry[0].HostID)
+	ts := collect_telemetry[0].Timestamp
+	filename := fmt.Sprintf("telemetry_logs/telemetry_%s_%d.json", host, ts)
+
+	// write file
+	err = os.WriteFile(filename, formatted_reports, 0644)
+	if err != nil {
+		fmt.Printf("Disk Write Error: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Successfully saved telemetry to: %s\n", filename)
+
 }
